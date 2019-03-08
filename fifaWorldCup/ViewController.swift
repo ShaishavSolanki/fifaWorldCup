e6d21560949806a26111775c047e549b23d65b87
shaishav solanki <shaishavsolanki@shaishavs-mac.local>
2019-03-07 20:25:27 -0800

    Initial Commit

---
+++ fifaWorldCup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fifaWorldCup
+//
+//  Created by Shaishav Solanki on 2019-03-07.
+//  Copyright © 2019 Shaishav Solanki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
